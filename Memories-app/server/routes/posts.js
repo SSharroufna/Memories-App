54163c32f4c6d9cc53090fc7b879a6538fd650cd
SARAH <sarah@alMBPalhasbsart.askey.com>
2023-09-24 18:53:48 -0600

    Added controllers folder

--- Memories-app/server/routes/posts.js
+++ Memories-app/server/routes/posts.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const router = express.Router();
+import { getPosts, createPost } from '../controllers/posts.js'
 
+const router = express.Router();
 
 //http://localhost:6000/posts
 
-router.get('/', (req,res) => {
-    res.send('THIS WORKS!!');
-});
+router.get('/', getPosts );
+router.post('/', createPost );
 
 export default router;
